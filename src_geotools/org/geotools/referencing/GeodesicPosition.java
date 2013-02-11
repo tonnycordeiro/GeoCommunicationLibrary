28a71beb722fc5870430967c30e63e5a420375ff
Renato <ravila.santos@gmail.com>
2013-02-11 09:26:09 -0200

    Adding geotools library

---
+++ src_geotools/org/geotools/referencing/GeodesicPosition.java
@@ -0,0 +1,25 @@
+package org.geotools.referencing;
+
+public class GeodesicPosition {
+	double X;
+	double Y;
+	public GeodesicPosition(double x, double y) {
+		super();
+		X = x;
+		Y = y;
+	}
+	public double getX() {
+		return X;
+	}
+	public void setX(double x) {
+		X = x;
+	}
+	public double getY() {
+		return Y;
+	}
+	public void setY(double y) {
+		Y = y;
+	}
+
+	
+}
